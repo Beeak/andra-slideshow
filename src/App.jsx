a986885cd2f1152342186c250142c61a1fba6fc8
Henry Kalvi Kaalma <dudetitan@gmail.com>
2024-02-21 13:05:16 +0200

    Esimene slide tehtud

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import React, { useRef, useState } from "react";
+import silinder from './media/20240221_103233.jpg';
+import silindri_valem from './media/valemid/silinder.png'
+import koonuse_valem from './media/valemid/koonus.png'
+
+import React, {  } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -15,7 +19,17 @@ export default function App() {
   return (
     <>
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide className='flex gap-2'>
+          <div>
+            <h1>Silinder ja Koonus</h1>
+            <img className='h-screen' src={silinder} alt="Slide 1" />
+          </div>
+          <div>
+            <h2>Asukoht: Kooli kõrval parklas</h2>
+            <img src={silindri_valem} alt="" />
+            <img src={koonuse_valem} alt="" />
+          </div>
+        </SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
